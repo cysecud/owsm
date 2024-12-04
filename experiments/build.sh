ede25037b5fd3226fc37bde59d135e699786b033
Massimiliano Baldo <bmaxium01@gmail.com>
2024-12-04 13:22:53 +0100

    feature: generate .tar files of docker images

--- experiments/build.sh
+++ experiments/build.sh
@@ -4,28 +4,42 @@
 # It generates images for datastore, oswm and opa with
 # all the files for testing purpose.
 
+EXPERIMENT="ex1"
+
 ## Datastore
 echo "Building \"datastore\" docker image"
 
-cp -r ./datas/ ../src/datastore
+cp -r ./artifacts/datas/ ../src/datastore
 docker build -t datastore ../src/datastore
+docker save -o datastore.tar datastore
+mv datastore.tar ./${EXPERIMENT}/images
 rm -rf ../src/datastore/datas
 
 ## Wrapper
 echo "Building \"wrapper\" docker image"
 
-cp -r ./policies/ ../src/wrapper
+cp -r ./artifacts/policies/ ../src/wrapper
 docker build -t wrapper ../src/wrapper
+docker save -o wrapper.tar wrapper
+mv wrapper.tar ./${EXPERIMENT}/images
 rm -rf ../src/wrapper/policies
 
 ## OPA
 echo "Building \"opa\" docker image"
 
-cp -r ./policies/ ./opa
-docker build -f opa/Dockerfile -t opa .
+cp -r ./artifacts/policies/ ./opa
+cp -r ./artifacts/inputs/ ./opa
+docker build -t opa ./opa/
+docker save -o opa.tar opa
+mv opa.tar ./${EXPERIMENT}/images
+rm -rf ./opa/inputs
 rm -rf ./opa/policies
 
 # Apache Benchmark
 echo "Building \"apache\" docker image"
 
-docker build -f apache/Dockerfile -t apache .
+cp -r ./artifacts/inputs/ ./apache
+docker build -t apache ./apache/
+docker save -o apache.tar apache
+mv apache.tar ./${EXPERIMENT}/images
+rm -rf ./apache/inputs
