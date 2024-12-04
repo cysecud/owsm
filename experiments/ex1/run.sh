4b31ff092a27671a91b3b23021eda9ab8c70c2d6
Massimiliano Baldo <bmaxium01@gmail.com>
2024-12-04 17:13:13 +0100

    fix: managment of docker compose

--- experiments/ex1/run.sh
+++ experiments/ex1/run.sh
@@ -9,14 +9,14 @@ INDEXES=("1" "2" "3")
 for index in "${INDEXES[@]}"; do
     echo "[INFO] Running experiment on use case n.$index"
     printf "POLICY_NAME=\"policy$index.rego\"\nDATA_NAME=\"data$index.json\"\nINPUT_NAME=\"input$index.json\"" > .env
-    docker compose up
+    docker compose up -d
 
     # Loop until the file exists
-    while [ ! -f ".done" ]; do
-        sleep 5  # Wait 5 seconds before checking again
+    while [ ! -f "./results/.done" ]; do
+        sleep 1  # Wait 5 seconds before checking again
     done
 
     docker compose stop
-    rm .env .done
+    rm .env ./results/.done
     echo "[INFO] Experiment on use case n.$index complete!"
 done
