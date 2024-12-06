f66d9b932dfb8f677940e9d2bbf847040d8bf8a2
Massimiliano Baldo <bmaxium01@gmail.com>
2024-12-06 13:44:14 +0100

    fix: path handling in main.py after being moved in dedicate directory

--- graphs/main.py
+++ graphs/main.py
@@ -3,44 +3,48 @@ import matplotlib.pyplot as plt
 import os
 from scipy.stats import mannwhitneyu
 
-# Step 1: Load the CSV data
-dir_path_1 = './ex1/results/'  # Replace with your file's path
+experiment_dirs = ["ex1", "ex2", "ex3"]
 
-print("|- Considering experiment 1")
-for file in os.listdir(dir_path_1):
-    data = pd.read_csv(os.path.join(dir_path_1, file))
+for exp in experiment_dirs:
+    
+    # Step 1: Load the CSV data
+    csv_path = f'../experiments/{exp}/results/'
 
-    # Step 2: Create a 'graphs' directory if it doesn't exist
-    output_dir = 'graphs/ex1'
-    os.makedirs(output_dir, exist_ok=True)
+    print(f"|- Considering experiment {exp}")
+    for file in os.listdir(csv_path):
+        data = pd.read_csv(os.path.join(csv_path, file))
 
-    # Step 3: Generate a box plot
-    plt.figure(figsize=(8, 6))
-    data[['OWSM', 'OPA']].boxplot()
-    plt.title('Box Plot')
-    #plt.yscale("log")
-    plt.savefig(os.path.join(output_dir, os.path.splitext(os.path.basename(file))[0] + '-boxplot.png'))
-    plt.close()
+        # Step 2: Create a 'graphs' directory if it doesn't exist
+        output_dir = './ex1'
+        os.makedirs(output_dir, exist_ok=True)
 
-    plt.figure(figsize=(8, 6))
-    plt.plot(data['number_request'], data['OWSM'], "o", color="red")  # Replace with relevant column names
-    plt.plot(data['number_request'], data['OPA'], "x", color="blue")
-    plt.title('Comparison ' + file)
-    ax=plt.gca()
-    ax.legend(["OWSM", "OPA"])
-    plt.xlabel('Number of request')  # Replace with relevant column name
-    plt.ylabel('Elapsed time (ms)')  # Replace with relevant column name
-    plt.savefig(os.path.join(output_dir, os.path.splitext(os.path.basename(file))[0] + '-plot.png'))
-    plt.close()
+        # Step 3: Generate a box plot
+        plt.figure(figsize=(8, 6))
+        data[['OWSM', 'OPA']].boxplot()
+        plt.title('Box Plot')
+        #plt.yscale("log")
+        plt.savefig(os.path.join(output_dir, os.path.splitext(os.path.basename(file))[0] + '-boxplot.png'))
+        plt.close()
 
-    print("|-- Plot saved")
+        plt.figure(figsize=(8, 6))
+        plt.plot(data['number_request'], data['OWSM'], "o", color="red")  # Replace with relevant column names
+        plt.plot(data['number_request'], data['OPA'], "x", color="blue")
+        plt.title('Comparison ' + file)
+        ax=plt.gca()
+        ax.legend(["OWSM", "OPA"])
+        plt.xlabel('Number of request')  # Replace with relevant column name
+        plt.ylabel('Elapsed time (ms)')  # Replace with relevant column name
+        plt.savefig(os.path.join(output_dir, os.path.splitext(os.path.basename(file))[0] + '-plot.png'))
+        plt.close()
 
-    # Perform the Mann-Whitney U test
-    result = mannwhitneyu(data['OWSM'], data['OPA'])
+        print("|-- Plot saved")
 
-    # Display the results
-    print("|-- U statistic:", result.statistic)
-    print("|-- P-value:", result.pvalue)
+        # Perform the Mann-Whitney U test
+        result = mannwhitneyu(data['OWSM'], data['OPA'])
+
+        # Display the results
+        print("|-- U statistic:", result.statistic)
+        print("|-- P-value:", result.pvalue)
 
 """ dir_path_2 = './ex2/results/' 
 
