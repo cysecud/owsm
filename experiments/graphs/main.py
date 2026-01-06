581ef7bcc1dd51a8a3a3deab304b496911040088
Massimiliano Baldo <bmaxium01@gmail.com>
2026-01-06 15:51:04 +0100

    refactor: removed old experiments

--- experiments/graphs/main.py
+++ experiments/graphs/main.py
@@ -11,141 +11,13 @@ plt.rc('xtick', labelsize=18)    # X-axis tick labels
 plt.rc('ytick', labelsize=18)    # Y-axis tick labels
 plt.rc('legend', fontsize=18)    # Legend font size
 plt.rc('font', size=10)          # Default font size
-
-def exp0b() :
-    exp = "exp-2l"
-
-    # Step 1: Load the CSV data
-    opa_csv_path = f"../bare-metal/results/{exp}/opa"
-    owsm_csv_path = f"../bare-metal/results/{exp}/owsm"
-
-    print(f"|- Considering experiment {exp}")
-
-    #data = pd.DataFrame(columns=["use_case", "num_requests", "mean_opa", "variance_opa", "mean_owsm", "variance_owsm"])
-
-    for file in os.listdir(opa_csv_path):
-        opa_data = pd.read_csv(os.path.join(opa_csv_path, file), sep='\t', header=0)
-        owsm_data = pd.read_csv(os.path.join(owsm_csv_path, file), sep='\t', header=0)
-
-        result = mannwhitneyu(opa_data["ttime"].tolist(), owsm_data["ttime"].tolist())
-        print("|-- U statistic:", result.statistic)
-        print("|-- P-value:", result.pvalue)
-
-        # Step 2: Create a 'graphs' directory if it doesn't exist
-        output_dir = './exp-0b'
-        os.makedirs(output_dir, exist_ok=True)
-
-        (base, _) = file.split(".")
-        (use_case, num_requests) = base.split("-")
-
-        #data.loc[len(data)] = {"use_case": int(use_case), "num_requests": int(num_requests), "mean_opa": opa_data["ttime"].mean(), "variance_opa": opa_data["ttime"].var(), "mean_owsm": owsm_data["ttime"].mean(), "variance_owsm": owsm_data["ttime"].var()}
-
-
-        data = pd.DataFrame({"number_request": range(1, len(owsm_data) + 1), "OWSM": owsm_data["ttime"], "OPA": opa_data["ttime"]})
-
-
-        # Step 3: Generate a box plot
-        plt.figure(figsize=(8, 6))
-        data[['OWSM', 'OPA']].boxplot()
-        plt.title('Box Plot')
-        #plt.yscale("log")
-        plt.savefig(os.path.join(output_dir, os.path.splitext(os.path.basename(file))[0] + '-boxplot.png'))
-        plt.close()
-
-        plt.figure(figsize=(8, 6))
-        plt.plot(data['number_request'], data['OWSM'], "o", color="red")  # Replace with relevant column names
-        plt.plot(data['number_request'], data['OPA'], "x", color="blue")
-        plt.title('Comparison ' + file)
-        ax=plt.gca()
-        ax.legend(["OWSM", "OPA"])
-        plt.xlabel('Number of request')  # Replace with relevant column name
-        plt.ylabel('Elapsed time (µs)')  # Replace with relevant column name
-        plt.savefig(os.path.join(output_dir, os.path.splitext(os.path.basename(file))[0] + '-plot.png'))
-        plt.close() 
-
-        print("|-- Plot saved")
-
-        # Perform the Mann-Whitney U test
-        #result = mannwhitneyu(data['OWSM'].tolist(), data['OPA'].tolist())
-        #stat, p_value = wilcoxon(data['OWSM'].tolist(), data['OPA'].tolist())
-
-        # Display the results
-        #print("|-- U statistic:", result.statistic)
-        #print("|-- P-value:", result.pvalue)
-
-        #print(f"Wilcoxon statistic: {stat}")
-        #print(f"P-value: {p_value}")
-
-def exp0() :
-    exp = "exp-1"
-
-    # Step 1: Load the CSV data
-    opa_csv_path = f"../bare-metal/results/{exp}/opa"
-    owsm_csv_path = f"../bare-metal/results/{exp}/owsm"
-
-    print(f"|- Considering experiment {exp}")
-
-    #data = pd.DataFrame(columns=["use_case", "num_requests", "mean_opa", "variance_opa", "mean_owsm", "variance_owsm"])
-
-    for file in os.listdir(opa_csv_path):
-        opa_data = pd.read_csv(os.path.join(opa_csv_path, file), sep='\t', header=0)
-        owsm_data = pd.read_csv(os.path.join(owsm_csv_path, file), sep='\t', header=0)
-
-        result = mannwhitneyu(opa_data["ttime"].tolist(), owsm_data["ttime"].tolist())
-        print("|-- U statistic:", result.statistic)
-        print("|-- P-value:", result.pvalue)
-
-        # Step 2: Create a 'graphs' directory if it doesn't exist
-        output_dir = './exp-0'
-        os.makedirs(output_dir, exist_ok=True)
-
-        (base, _) = file.split(".")
-        (use_case, num_requests) = base.split("-")
-
-        #data.loc[len(data)] = {"use_case": int(use_case), "num_requests": int(num_requests), "mean_opa": opa_data["ttime"].mean(), "variance_opa": opa_data["ttime"].var(), "mean_owsm": owsm_data["ttime"].mean(), "variance_owsm": owsm_data["ttime"].var()}
-
-
-        data = pd.DataFrame({"number_request": range(1, len(owsm_data) + 1), "OWSM": owsm_data["ttime"], "OPA": opa_data["ttime"]})
-
-
-        # Step 3: Generate a box plot
-        plt.figure(figsize=(8, 6))
-        data[['OWSM', 'OPA']].boxplot()
-        plt.title('Box Plot')
-        #plt.yscale("log")
-        plt.savefig(os.path.join(output_dir, os.path.splitext(os.path.basename(file))[0] + '-boxplot.png'))
-        plt.close()
-
-        plt.figure(figsize=(8, 6))
-        plt.plot(data['number_request'], data['OWSM'], "o", color="red")  # Replace with relevant column names
-        plt.plot(data['number_request'], data['OPA'], "x", color="blue")
-        plt.title('Comparison ' + file)
-        ax=plt.gca()
-        ax.legend(["OWSM", "OPA"])
-        plt.xlabel('Number of request')  # Replace with relevant column name
-        plt.ylabel('Elapsed time (µs)')  # Replace with relevant column name
-        plt.savefig(os.path.join(output_dir, os.path.splitext(os.path.basename(file))[0] + '-plot.png'))
-        plt.close() 
-
-        print("|-- Plot saved")
-
-        # Perform the Mann-Whitney U test
-        #result = mannwhitneyu(data['OWSM'].tolist(), data['OPA'].tolist())
-        #stat, p_value = wilcoxon(data['OWSM'].tolist(), data['OPA'].tolist())
-
-        # Display the results
-        #print("|-- U statistic:", result.statistic)
-        #print("|-- P-value:", result.pvalue)
-
-        #print(f"Wilcoxon statistic: {stat}")
-        #print(f"P-value: {p_value}")
        
 def exp1() :
     exp = "exp-1"
 
     # Step 1: Load the CSV data
-    opa_csv_path = f"../bare-metal/results/{exp}/opa"
-    owsm_csv_path = f"../bare-metal/results/{exp}/owsm"
+    opa_csv_path = f"../results/{exp}/opa"
+    owsm_csv_path = f"../results/{exp}/owsm"
 
     print(f"|- Considering experiment {exp}")
 
@@ -160,7 +32,7 @@ def exp1() :
         print("|-- P-value:", result.pvalue) """
 
         # Step 2: Create a 'graphs' directory if it doesn't exist
-        output_dir = './exp-1'
+        output_dir = '../results/exp-1'
         os.makedirs(output_dir, exist_ok=True)
 
         (base, _) = file.split(".")
@@ -179,39 +51,6 @@ def exp1() :
             "iqr_owsm": (owsm_data["ttime"].quantile(0.75) - owsm_data["ttime"].quantile(0.25)),
         }
 
-        # Step 3: Generate a box plot
-        """ plt.figure(figsize=(8, 6))
-        data[['OWSM', 'OPA']].boxplot()
-        plt.title('Box Plot')
-        #plt.yscale("log")
-        plt.savefig(os.path.join(output_dir, os.path.splitext(os.path.basename(file))[0] + '-boxplot.png'))
-        plt.close()
-
-        plt.figure(figsize=(8, 6))
-        plt.plot(data['number_request'], data['OWSM'], "o", color="red")  # Replace with relevant column names
-        plt.plot(data['number_request'], data['OPA'], "x", color="blue")
-        plt.title('Comparison ' + file)
-        ax=plt.gca()
-        ax.legend(["OWSM", "OPA"])
-        plt.xlabel('Number of request')  # Replace with relevant column name
-        plt.ylabel('Elapsed time (µs)')  # Replace with relevant column name
-        plt.savefig(os.path.join(output_dir, os.path.splitext(os.path.basename(file))[0] + '-plot.png'))
-        plt.close() 
-
-        print("|-- Plot saved")
-
-        # Perform the Mann-Whitney U test
-        result = mannwhitneyu(data['OWSM'].tolist(), data['OPA'].tolist())
-        stat, p_value = wilcoxon(data['OWSM'].tolist(), data['OPA'].tolist())
-
-        # Display the results
-        print("|-- U statistic:", result.statistic)
-        print("|-- P-value:", result.pvalue)
-
-        print(f"Wilcoxon statistic: {stat}")
-        print(f"P-value: {p_value}")
-        """
-
     for use_case in range(1,4):    
         use_case_data = data[data["use_case"] == use_case].copy().sort_values(by='num_requests')[:3]
         use_case_data.loc[len(use_case_data)] = {
@@ -228,10 +67,6 @@ def exp1() :
         use_case_data = use_case_data.sort_values(by='num_requests')
 
         print(linregress([1,2,3,4],[1,1,1,1]))
-
-        # use_case_data["log_num_requests"] = [math.log(val) for val in use_case_data["num_requests"]]
-
-        #print(use_case_data)
 
         result = linregress(use_case_data["num_requests"], use_case_data["mean_owsm"])
         print("|-- Use case:", use_case)
@@ -279,91 +114,11 @@ def exp1() :
         plt.close()
 
 def exp2():
-    exp = "exp-2"
-
-    # Step 1: Load the CSV data
-    opa_csv_path = f"../bare-metal/results/{exp}/opa"
-    owsm_csv_path = f"../bare-metal/results/{exp}/owsm"
-
-    print(f"|- Considering experiment {exp}")
-
-
-    data = pd.DataFrame(columns=["use_case", "concurrency", "mean_opa", "variance_opa", "iqr_opa", "mean_owsm", "variance_owsm", "iqr_owsm"])
-
-    for file in os.listdir(opa_csv_path):
-        opa_data = pd.read_csv(os.path.join(opa_csv_path, file), sep='\t', header=0)
-        owsm_data = pd.read_csv(os.path.join(owsm_csv_path, file), sep='\t', header=0)
-
-        result = mannwhitneyu(opa_data["ttime"].tolist(), owsm_data["ttime"].tolist())
-        print("|-- U statistic:", result.statistic)
-        print("|-- P-value:", result.pvalue)
-
-        # Step 2: Create a 'graphs' directory if it doesn't exist
-        output_dir = './exp-2'
-        os.makedirs(output_dir, exist_ok=True)
-
-        (base, _) = file.split(".")
-        (use_case, concurrency) = base.split("-")
-
-        data.loc[len(data)] = {
-            "use_case": int(use_case),
-            "concurrency": int(concurrency),
-            "mean_opa": opa_data["ttime"].mean(),
-            "variance_opa": opa_data["ttime"].var(),
-            "iqr_opa": (opa_data["ttime"].quantile(0.75) - opa_data["ttime"].quantile(0.25)),
-            "mean_owsm": owsm_data["ttime"].mean(), 
-            "variance_owsm": owsm_data["ttime"].var(),
-            "iqr_owsm": (owsm_data["ttime"].quantile(0.75) - owsm_data["ttime"].quantile(0.25)),
-            }
-
-    for use_case in range(1,4):
-        use_case_data = data[data["use_case"] == use_case].copy().sort_values(by='concurrency')
-
-        use_case_data['std_dev_opa'] = use_case_data['variance_opa'] ** 0.5
-        use_case_data['std_dev_owsm'] = use_case_data['variance_owsm'] ** 0.5
-
-        print(use_case_data)
-        # Plot
-        plt.figure(figsize=(12, 7))
-
-        # Plot OPA
-        plt.errorbar(
-            use_case_data['concurrency'],        # x-axis
-            use_case_data['mean_opa'],            # y-axis
-            yerr=use_case_data['iqr_opa'],    # Error bars (OPA std deviation)
-            fmt='o--',                  # Line and marker style
-            label='OPA'
-        )
-
-        # Plot OWSM
-        plt.errorbar(
-            use_case_data['concurrency'],        # x-axis
-            use_case_data['mean_owsm'],           # y-axis
-            yerr=use_case_data['iqr_owsm'],   # Error bars (OWSM std deviation)
-            fmt='s--',                 # Line and marker style (different for clarity)
-            label='OWSM'
-        )
-
-        # Set logarithmic scale for x-axis
-        #plt.xscale('log')
-        #plt.yscale('log')
-
-        # Labels and title
-        plt.xlabel('Level of concurrency')
-        plt.ylabel('Time per request (µs)')
-        plt.title(f'OPA vs OWSM (use case {use_case})')
-        plt.legend()
-        plt.grid(True, which="both", linestyle='--', linewidth=0.5)
-
-        plt.savefig(os.path.join(output_dir, str(use_case) + '-plot.png'))
-        plt.close()
-
-def exp2l():
     exp = "exp-2l"
 
     # Step 1: Load the CSV data
-    opa_csv_path = f"../bare-metal/results/{exp}/opa"
-    owsm_csv_path = f"../bare-metal/results/{exp}/owsm"
+    opa_csv_path = f"../results/{exp}/opa"
+    owsm_csv_path = f"../results/{exp}/owsm"
 
     print(f"|- Considering experiment {exp}")
 
@@ -374,7 +129,7 @@ def exp2l():
         owsm_data = pd.read_csv(os.path.join(owsm_csv_path, file), sep='\t', header=0)
 
         # Step 2: Create a 'graphs' directory if it doesn't exist
-        output_dir = './exp-2l'
+        output_dir = '../results/exp-2'
         os.makedirs(output_dir, exist_ok=True)
 
         (base, _) = file.split(".")
@@ -441,8 +196,5 @@ def exp2l():
         plt.savefig(os.path.join(output_dir, str(use_case) + '-plot.png'))
         plt.close()
 
-#exp0()
 exp1()
-#exp2()
-#exp2l()
-#exp0b()
+exp2()
